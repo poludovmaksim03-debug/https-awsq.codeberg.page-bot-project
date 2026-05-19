a7afda408541962e597e060fdc5765703240514a
poludovmaksim03-debug <poludovmaksim03@gmail.com>
2026-05-19 20:39:15 +0400

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -358,6 +358,10 @@ class ChatBot {
 
 export default ChatBot;
 
+// Инициализация при загрузке страницы
+    document.addEventListener('DOMContentLoaded', init());
+    console.log(typeof Tesseract)
+
 
 
 
